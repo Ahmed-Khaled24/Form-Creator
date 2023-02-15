353cd602f98bd95b77a295993cd4ff7076a7a242
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-16 00:02:12 +0200

    feat: changes to general files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Navbar from './components/navbar/Navbar.component';
+import Form from './components/form/Form.component';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Fragment>
+			<Navbar
+				brand={{ smallText: 'The Ultimate', largeText: 'Form Creator' }}
+				navLinks={['Creator', 'Viewer']}
+			/>
+			<Form/>
+		</Fragment>
+	);
 }
 
 export default App;
