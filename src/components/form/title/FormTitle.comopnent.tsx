e87af6116fa49291c408db55897fd131ee0e23b5
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-16 01:30:54 +0200

    feat: finish form title component

---
+++ src/components/form/title/FormTitle.comopnent.tsx
@@ -0,0 +1,23 @@
+import { RenderMode } from '../../../types/FormData.type';
+import { EditableTitle, Title } from './FormTitle.styles';
+
+type Props = {
+    setTitle: (title: string) => void,
+    title: string,
+    mode: RenderMode,
+}
+
+const FormTitle = ({setTitle, title, mode}: Props) => {
+    function handleChangeTitle(e: React.ChangeEvent<HTMLInputElement>){
+        const newTitle = e.target.value;
+        setTitle(newTitle);
+    }
+    return (
+        mode === RenderMode.edit ? (
+            <EditableTitle placeholder='Type form title' onChange={handleChangeTitle}/>
+        ):
+        <Title> {title} </Title>
+    )
+}
+
+export default FormTitle
---
+++ src/components/form/title/FormTitle.styles.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+
+const width = 900;
+const fontSize = 32;
+
+ export const EditableTitle = styled.input`
+    width: ${width}px;
+    padding: 5px 20px;
+    border: none;
+    border-bottom: 2px solid white;
+    background-color: transparent;
+    color: white;
+    font-size: ${fontSize}px;
+
+    &:focus-visible {
+        outline: none;
+    }
+ `
+
+ export const Title = styled.p `
+    width: ${width}px;
+    padding: 5px 20px;
+    border: none;
+    border-bottom: 2px solid white;
+    background-color: transparent;
+    color: white;
+    font-size: ${fontSize}px;
+    letter-spacing: -1px;
+ `
