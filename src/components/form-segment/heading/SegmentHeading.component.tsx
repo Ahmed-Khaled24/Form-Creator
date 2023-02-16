0bb14cac792130e74641904c6acc1d3090fddf53
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-16 22:13:23 +0200

    feat: create segment heading component

---
+++ src/components/form-segment/heading/SegmentHeading.component.tsx
@@ -0,0 +1,50 @@
+import { Fragment } from 'react';
+import { RenderMode } from '../../../types/FormData.type';
+import { SegmentType } from '../../../types/FormSegmentData.type';
+import { EditableQuestion, HeadingDiv, Question, QuestionCondition, SelectQuestionType } from './SegmentHeading.styles';
+type Props = {
+    question: string,
+    mode: RenderMode,
+    required: boolean,
+    parentSegmentId: number,
+    changeSegmentType: (segmentId: number, type: SegmentType) => void,
+    changeQuestionText: (segmentId: number, question: string) => void,
+}
+
+const SegmentHeading = ({question, mode, required, parentSegmentId, changeSegmentType, changeQuestionText}: Props) => {
+
+    function handleChangeQuestionType(e: React.ChangeEvent<HTMLSelectElement>){
+        const newType = Number(e.target.value);
+        changeSegmentType(parentSegmentId, newType);
+    }
+
+    function handleChangeQuestionText(e: React.ChangeEvent<HTMLInputElement>){
+        const newQuestion = e.target.value;
+        changeQuestionText(parentSegmentId, newQuestion);
+    }
+
+    return (
+        <HeadingDiv>
+            {
+                mode === RenderMode.edit ? ( 
+                    <Fragment>
+                        <EditableQuestion type='text' placeholder='Type your question' onChange={handleChangeQuestionText}/>
+                        <SelectQuestionType onChange={handleChangeQuestionType}>
+                            <option value={SegmentType.shortAnswer}> short answer </option>
+                            <option value={SegmentType.paragraph}> paragraph </option>
+                            <option value={SegmentType.multipleChoice}> multiple choice </option>
+                            <option value={SegmentType.multiSelect}> multi select </option>
+                        </SelectQuestionType>
+                    </Fragment>
+                ) : (
+                    <Fragment>
+                        <Question> {question} </Question>
+                        <QuestionCondition> {required?'Required':'Optional'} </QuestionCondition>
+                    </Fragment> 
+                )
+            }
+        </HeadingDiv>
+    )
+}
+
+export default SegmentHeading;
---
+++ src/components/form-segment/heading/SegmentHeading.styles.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const HeadingDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const EditableQuestion = styled.input`
+    border: none;
+    font-family: inherit;
+    font-size: 32px;
+    padding: 5px 20px;
+`
+
+export const SelectQuestionType = styled.select`
+    padding: 5px 20px;
+`
+
+export const Question = styled.p`
+    font-size: 32px;
+    letter-spacing: -1px;
+`
+
+export const QuestionCondition = styled.p`
+    color: grey:
+    font-size: 16px;
+`
